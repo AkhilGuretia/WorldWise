e414454b6fe5890f2b880064e256da08bca56617
Akhil Kumar <akhilguretia@gmail.com>
2023-12-02 23:06:43 +0530

    Build Homepage and use CSS modules to style the app

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,25 @@
 import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 const Homepage = () => {
   return (
     <div>
       <PageNav />
-      <h1>WorldWise</h1>
+
+      <main className={styles.homepage}>
+        <section>
+          <h1>
+            You travel the World.
+            <br />
+            Worldwise keeps track of your of your adventures.
+          </h1>
+          <h2>
+            A world map that tracks your footsteps into every city you can think
+            of. Never forget your wonderful experiences, and show your friends
+            how you have wandered the world.
+          </h2>
+        </section>
+      </main>
     </div>
   );
 };
