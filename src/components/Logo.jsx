135567cffc80e33675d73a43999af260603c68b0
Akhil Kumar <akhilguretia@gmail.com>
2023-12-03 14:46:17 +0530

    Apply styles to PageNav using css modules

---
+++ src/components/Logo.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./Logo.module.css";
+
+const Logo = () => {
+  return (
+    <Link to="/">
+      <img
+        src="src/assets/logo.png"
+        alt="Worldwise Logo"
+        className={styles.logo}
+      />
+    </Link>
+  );
+};
+
+export default Logo;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,24 +3,21 @@ import styles from "./Homepage.module.css";
 
 const Homepage = () => {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNav />
-
-      <main className={styles.homepage}>
-        <section>
-          <h1>
-            You travel the World.
-            <br />
-            Worldwise keeps track of your of your adventures.
-          </h1>
-          <h2>
-            A world map that tracks your footsteps into every city you can think
-            of. Never forget your wonderful experiences, and show your friends
-            how you have wandered the world.
-          </h2>
-        </section>
-      </main>
-    </div>
+      <section>
+        <h1>
+          You travel the World.
+          <br />
+          Worldwise keeps track of your of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
   );
 };
 
