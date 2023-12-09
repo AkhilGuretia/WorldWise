e04db1bdde7291ffc6294f59b4a9381497ca3b9f
Akhil Kumar <akhilguretia@gmail.com>
2023-12-09 22:30:11 +0530

    set up a json server to fetch cities and countries data from fake api

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react/prop-types
+const CityItem = ({ city }) => {
+  return <li>{city.cityName}</li>;
+};
+
+export default CityItem;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,5 +1,38 @@
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const CityList = () => {
-  return <div>CityList</div>;
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetch("http://localhost:3000/cities");
+
+        const res = await data.json();
+
+        setCities(res);
+      } catch (error) {
+        console.log("something went wrong while fetching the data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 };
 
 export default CityList;
