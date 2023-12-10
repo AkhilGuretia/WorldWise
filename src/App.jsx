1811e2b9c6a671860d092dc709dd7d76fc2a628e
Akhil Kumar <akhilguretia@gmail.com>
2023-12-10 22:57:17 +0530

    Lift up state to Parent component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,31 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetch("http://localhost:3000/cities");
+
+        const res = await data.json();
+
+        setCities(res);
+      } catch (error) {
+        console.log("something went wrong while fetching the data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,8 +40,14 @@ const App = () => {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<CountryList />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,31 +1,8 @@
+/* eslint-disable react/prop-types */
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
-import { useEffect } from "react";
-import { useState } from "react";
 
-const CityList = () => {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        setIsLoading(true);
-        const data = await fetch("http://localhost:3000/cities");
-
-        const res = await data.json();
-
-        setCities(res);
-      } catch (error) {
-        console.log("something went wrong while fetching the data");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getData();
-  }, []);
-
+const CityList = ({ cities, isLoading }) => {
   return (
     <ul className={styles.CityList}>
       {cities.map((city) => (
