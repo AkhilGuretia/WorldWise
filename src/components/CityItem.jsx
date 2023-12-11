94c77b58080037d904b3e32e961e242db74b5ab6
Akhil Kumar <akhilguretia@gmail.com>
2023-12-11 21:36:49 +0530

    Finish Building the CityList component

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,24 @@
-// eslint-disable-next-line react/prop-types
+/* eslint-disable react/prop-types */
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
 const CityItem = ({ city }) => {
-  return <li>{city.cityName}</li>;
+  const { emoji, cityName, date } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
 };
 
 export default CityItem;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable react/prop-types */
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
 
 const CityList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+
+  if (cities.length < 1)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
   return (
-    <ul className={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city) => (
         <CityItem city={city} key={city.id} />
       ))}
